b2ed05e255f3f4b5eee0e20c22c39daa366ef2fe
julian <julian.patino@sofwareone.com>
2023-10-05 12:10:13 -0500

    Se colocan como comentarios los pasos pedidos en la prueba

--- src/main/java/pruebaSelenium.java
+++ src/main/java/pruebaSelenium.java
@@ -18,18 +18,22 @@ public class pruebaSelenium {
         
         driver.manage().window().maximize();
         driver.get("https://www.google.com");
-
+        
+        //Buscar en Google la palabra “automatización”
         WebElement searchBox = driver.findElement(By.name("q"));
         searchBox.sendKeys("automatización");
         searchBox.sendKeys(Keys.ENTER);
         
+        //Buscar el link de la Wikipedia resultante
         WebElement wiki = driver.findElement(By.xpath("//h3[normalize-space()='Automatización - Wikipedia, la enciclopedia libre']"));
         wiki.click();
         
+        //Comprobar en que año se hizo el primer proceso automatico
         WebElement elemento = driver.findElement(By.xpath("//*[@id=\"mw-content-text\"]/div[1]/p[33]"));
         String textoCapturado = elemento.getText();
         System.out.println("Texto capturado: " + textoCapturado);
         
+        //Realizar un screenshot de la página de la Wikipedia
         File screenshotFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
         FileUtils.copyFile(screenshotFile, new File("captura.png"));
         
